22f93bddb8c41290c321379703d45a8a58be8cfc
nhd98z <nhd98z@gmail.com>
2023-06-07 09:44:55 +0700

    ok

--- src/hooks/useKyberSwapConfig.ts
+++ src/hooks/useKyberSwapConfig.ts
@@ -68,7 +68,7 @@ const parseGlobalResponse = (
   const aggregatorDomain = data?.aggregator ?? AGGREGATOR_API
   const isEnableAuthenAggregator = !data ? true : !!data?.isEnableAuthenAggregator
   return {
-    aggregatorDomain,
+    aggregatorDomain: 'https://aggregator-api.stg.kyberengineering.io',
     aggregatorAPI: `${aggregatorDomain}/${NETWORKS_INFO[chainId].aggregatorRoute}/route/encode`,
     isEnableAuthenAggregator,
   }
